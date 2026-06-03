8cdd962d2f826e3070ee28733118590e90455d7a
markusxau-dot <markusxau@gmail.com>
2026-06-03 02:40:02 -0300

    app.py

--- app.py
+++ app.py
@@ -66,8 +66,8 @@ st.markdown("""
     /* REFRESH BUTTON POSITION */
     div.stButton > button {
         position: fixed !important;
-        right: 25px !important;
-        bottom: 135px !important;
+        right: 0px !important;
+        bottom: 80px !important;
         z-index: 999999 !important;
         border-radius: 50% !important;
         width: 54px !important;
